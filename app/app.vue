416ce4aee735176e6ff5cb4c0f54a7159142445d
esandorfi <e.sandorfi@gmail.com>
2025-07-15 23:25:38 +0200

    speed insights

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,11 @@
 <template>
   <UApp>
     <Analytics />
+    <SpeedInsights />
     <NuxtPage />
   </UApp>
 </template>
 <script setup lang="ts">
 import { Analytics } from "@vercel/analytics/nuxt";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
